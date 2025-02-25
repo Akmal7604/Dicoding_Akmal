c0e60a2dc6dc78c4f2ec17428d6e42becc74b8fd
Akmal7604 <155626273+Akmal7604@users.noreply.github.com>
2025-02-25 13:31:03 +0000

    251

--- belajar_python1.py
+++ belajar_python1.py
@@ -1,3 +1,5 @@
+
+# Union & Intersection in set
 set1 = {1, 2, 3, 4, 5}
 set2 = {4, 5, 6, 7, 8}
 
@@ -6,3 +8,8 @@ print("Union:", union)
 
 intersection = set1.intersection(set2)
 print("Intersection:", intersection)
+
+# array
+a = [1,2,3,4]
+del a[0]
+print('a:', a)
